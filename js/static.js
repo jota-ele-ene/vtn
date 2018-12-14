574c918cd58c549c8c075b5da1a8ee4759e716af
jota-ele-ene <hi@jln.bz>
2018-12-14 21:28:21 +0000

    Background Image refresh button

--- js/static.js
+++ js/static.js
@@ -1,14 +1,30 @@
-var app = new Vue({
+var appTitle = new Vue({
     el: '.title',
     data: {
-    greeting: 'Hello World!',
-    background: 'http://source.unsplash.com/random'
+        greeting: 'Hello World!'
+    }
+})
+
+var appBackground = new Vue({
+    el: '.bg',
+    data: {
+        image: "https://source.unsplash.com/random"
     },
     methods: {
-    humanizeURL: function (url) {
-        return url
-        .replace(/^https?:\/\//, '')
-        .replace(/\/$/, '')
-    }
-    }
+        humanizeURL(url) {
+            return url
+            .replace(/^https?:\/\//, '')
+            .replace(/\/$/, '')
+        },
+        refreshBackground ()
+        {
+            this.image = "https://source.unsplash.com/random?=" + new Date().getTime()
+        }
+
+    },
+    computed: {
+        background() {
+            return "{ background-image: url(https://source.unsplash.com/random?="+ new Date().getTime() +");}"
+        }
+    },
 })
